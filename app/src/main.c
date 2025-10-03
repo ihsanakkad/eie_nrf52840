17705d10f7b3dddd886c64a5491553202d6e0235
IhsanA <hassounakkad216@gmail.com>
2025-10-02 19:58:34 -0600

    fix

--- app/src/main.c
+++ app/src/main.c
@@ -3,7 +3,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-#define LEDO_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
